fd04ff5390ef013a58698ed2ca1842d1620919c8
David Drobnak <david.drobnak26@gmail.com>
2024-07-13 13:35:22 +0200

    feat: add admin to subscription struct

--- src/Registry.sol
+++ src/Registry.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 error Registry__TooLittleETH();
 error Registry__OnlyOperator();
+error Registry__OnlyProtocolAdmin();
 
 contract Registry {
     uint256 public PRICE_PER_CONTRACT_SCAN = 1 gwei;
@@ -11,7 +12,9 @@ contract Registry {
     struct Subscription {
         address[] scope;
         uint256 scans;
+        address admin;
     }
+
     mapping(bytes32 protocolId => Subscription subscription) public protocolMapping;
 
     event ProtocolAdded(bytes32 protocolId);
@@ -27,18 +30,32 @@ contract Registry {
         _;
     }
 
-    function registerProtocol(bytes32 _protocolId, address[] calldata _scope, uint256 _scans) external payable {
+    modifier onlyProtocolAdmin(bytes32 _protocolId) {
+        if (msg.sender != protocolMapping[_protocolId].admin) revert Registry__OnlyProtocolAdmin();
+        _;
+    }
+
+    function registerProtocol(bytes32 _protocolId, address[] calldata _scope, uint256 _scans, address _admin) external payable {
         uint256 price = getPrice(_scope.length, _scans);
         if (msg.value < price) revert Registry__TooLittleETH();
 
-        protocolMapping[_protocolId] = Subscription(_scope, _scans);
+        protocolMapping[_protocolId] = Subscription(_scope, _scans, _admin);
 
         emit ProtocolAdded(_protocolId);
     }
 
-    function getSubscription(bytes32 protocolId) external view returns (address[] memory scope, uint256 scans) {
+    function getSubscription(bytes32 protocolId) external view returns (address[] memory scope, uint256 scans, address admin) {
         Subscription storage subscription = protocolMapping[protocolId];
-        return (subscription.scope, subscription.scans);
+        return (subscription.scope, subscription.scans, subscription.admin);
+    }
+
+    function updateSubscription(bytes32 protocolId, address[] calldata _scope, uint256 _scans) external payable onlyProtocolAdmin(protocolId) {
+        uint256 price = getPrice(_scope.length, _scans);
+        if (msg.value < price) revert Registry__TooLittleETH();
+
+        Subscription storage subscription = protocolMapping[protocolId];
+        subscription.scope = _scope;
+        subscription.scans = _scans;
     }
 
     function isSubscribed(bytes32 protocolId) public view returns (bool) {
--- test/Registry.t.sol
+++ test/Registry.t.sol
@@ -12,6 +12,7 @@ contract RegistryTest is Test {
 
     address alice = makeAddr("alice");
     address operator = makeAddr("operator");
+    address protocolAdmin = makeAddr("protocol admin");
 
     function setUp() public {
         vm.prank(operator);
@@ -28,7 +29,7 @@ contract RegistryTest is Test {
         uint256 pricePerContractScan = registry.PRICE_PER_CONTRACT_SCAN();
         uint256 price = SCANS * scope.length * pricePerContractScan;
 
-        registry.registerProtocol{ value: price }(protocolId, scope, SCANS);
+        registry.registerProtocol{ value: price }(protocolId, scope, SCANS, protocolAdmin);
 
         return (scope, SCANS);
     }
@@ -37,7 +38,7 @@ contract RegistryTest is Test {
         uint256 pricePerContractScan = registry.PRICE_PER_CONTRACT_SCAN();
         uint256 price = _scans * _scope.length * pricePerContractScan;
 
-        registry.registerProtocol{ value: price }(protocolId, _scope, _scans);
+        registry.registerProtocol{ value: price }(protocolId, _scope, _scans, protocolAdmin);
 
         return (_scope, _scans);
     }
@@ -45,10 +46,11 @@ contract RegistryTest is Test {
     function test_registerProtocol() public {
         _registerSampleProtocol();
 
-        (address[] memory scopeRegistry, uint256 scansRegistry) = registry.getSubscription(protocolId);
+        (address[] memory scopeRegistry, uint256 scansRegistry, address admin) = registry.getSubscription(protocolId);
         assertEq(scansRegistry, 100);
         assertEq(scopeRegistry[0], address(0));
         assertEq(scopeRegistry[1], address(1));
+        assertEq(admin, protocolAdmin);
     }
 
     function test_onlyOperator() public {
@@ -60,6 +62,20 @@ contract RegistryTest is Test {
         vm.expectRevert(Registry__OnlyOperator.selector);
         vm.prank(alice);
         registry.postScan(protocolIds);
+    }
+
+    function test_onlyProtocolAdmin() public {
+        _registerSampleProtocol();
+
+        address[] memory newScope = new address[](1);
+        newScope[0] = address(0);
+
+        uint256 newScans = 21;
+
+
+        vm.expectRevert(Registry__OnlyProtocolAdmin.selector);
+        vm.prank(alice);
+        registry.updateSubscription(protocolId, newScope, newScans);
     }
 
     function test_subscription() public {
@@ -77,14 +93,15 @@ contract RegistryTest is Test {
         vm.prank(operator);
         registry.postScan(protocolIds);
 
-        (, uint256 scansRemaining) = registry.getSubscription(protocolId);
+        (, uint256 scansRemaining, ) = registry.getSubscription(protocolId);
         assertEq(scansRemaining, 0);
 
         vm.prank(operator);
         registry.postScan(protocolIds);
 
-        (address[] memory scopeAfterDeletion, uint256 scansAfterDeletion) = registry.getSubscription(protocolId);
+        (address[] memory scopeAfterDeletion, uint256 scansAfterDeletion, address adminAfterDeletion) = registry.getSubscription(protocolId);
         assertEq(scopeAfterDeletion.length, 0);
         assertEq(scansAfterDeletion, 0);
+        assertEq(adminAfterDeletion, address(0));
     }
 }
